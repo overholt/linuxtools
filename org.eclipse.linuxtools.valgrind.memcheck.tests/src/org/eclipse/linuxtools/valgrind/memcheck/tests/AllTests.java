913c3f31e2fd74a4993b28eda0984c0947aa76d4
Andrew Overholt <overholt@redhat.com>
2008-12-10 22:44:50 +0000

    Add suite and test.xml

---
+++ org.eclipse.linuxtools.valgrind.memcheck.tests/src/org/eclipse/linuxtools/valgrind/memcheck/tests/AllTests.java
@@ -0,0 +1,17 @@
+package org.eclipse.linuxtools.valgrind.memcheck.tests;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class AllTests {
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite(
+				"Tests for org.eclipse.linuxtools.valgrind.memcheck");
+		// $JUnit-BEGIN$
+		suite.addTestSuite(BasicTest.class);
+		// $JUnit-END$
+		return suite;
+	}
+
+}
