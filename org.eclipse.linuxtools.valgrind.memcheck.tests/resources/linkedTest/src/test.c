50ea1dc14e65cc0dd6dc531a351cae2d83db63d6
Elliott Baron <ebaron@fedoraproject.org>
2009-03-18 21:08:28 +0000

    Fixing resources

---
+++ org.eclipse.linuxtools.valgrind.memcheck.tests/resources/linkedTest/src/test.c
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2008 Red Hat, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Elliott Baron <ebaron@redhat.com> - initial API and implementation
+ *******************************************************************************/
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+
+#define SIZE 10
+int main() {
+	char *waste = (char *)malloc(sizeof(char) * SIZE);
+	int *a;
+	printf("%d\n", *a);
+	waste[SIZE] = 0;
+	return 0;
+}
